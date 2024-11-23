8d710ae7d4da158341b7817eff10a55686756a4d
naissky <outdaywork@gmail.com>
2024-11-23 15:33:55 -0500

    disable menu

--- app/components/ui/NavLinks.tsx
+++ app/components/ui/NavLinks.tsx
@@ -6,7 +6,7 @@ export default function NavLinks(){
             <ul className="flex flex-row px-5 md:px-20 py-5 items-center justify-center gap-5 md:gap-10 rounded-xl shadow-lg">
                 <li><Link href="/" className="hover:underline md:text-xl font-semibold">Home</Link></li>
                 <li><Link href="/about" className="hover:underline md:text-xl font-semibold">About</Link></li>
-                <li><Link href="/menu" className="hover:underline md:text-xl font-semibold">Menu</Link></li>
+                <li><Link href="/" className="text-gray-500 md:text-xl font-semibold pointer-events-none">Menu</Link></li>
                 <li><Link href="/contact" className="hover:underline md:text-xl font-semibold">Contact</Link></li>
             </ul>
         </div>
